93368a8718618a56c9c768a51d600edaf3352852
asielp3D <asielmasespacio@gmail.com>
2023-12-01 13:09:05 +0100

    script rayo

--- Assets/Scripts/RayoMagico.cs
+++ Assets/Scripts/RayoMagico.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RayoMagico : MonoBehaviour
 {
     public Camera camaraPrincipal;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -32,17 +34,67 @@ public class RayoMagico : MonoBehaviour
             if(hitRayo.transform.gameObject.layer == 6)
             {
                 Debug.Log("impacto bola");
+                StartCoroutine(TimerCoroutine1());
+                
+                
             }
 
             if(hitRayo.transform.gameObject.layer == 7)
             {
                 Debug.Log("impacto cubo grande");
+                StartCoroutine(TimerCoroutine2());
             }
 
             if(hitRayo.transform.gameObject.layer == 8)
             {
                 Debug.Log("impacto cubo pequeño");
+                StartCoroutine(TimerCoroutine3());
             }
         } 
+    }
+
+    IEnumerator TimerCoroutine1()
+    {
+        Debug.Log("5");
+        yield return new WaitForSeconds (1);
+        Debug.Log("4");
+        yield return new WaitForSeconds (1);
+        Debug.Log("3");
+        yield return new WaitForSeconds (1);
+        Debug.Log("2");
+        yield return new WaitForSeconds (1);
+        Debug.Log("1");
+        yield return new WaitForSeconds (1);
+        SceneManager.LoadScene(1);
+    }
+
+    IEnumerator TimerCoroutine2()
+    {
+        Debug.Log("5");
+        yield return new WaitForSeconds (1);
+        Debug.Log("4");
+        yield return new WaitForSeconds (1);
+        Debug.Log("3");
+        yield return new WaitForSeconds (1);
+        Debug.Log("2");
+        yield return new WaitForSeconds (1);
+        Debug.Log("1");
+        yield return new WaitForSeconds (1);
+        SceneManager.LoadScene(2);
+    }
+
+    IEnumerator TimerCoroutine3()
+    {
+        Debug.Log("5");
+        yield return new WaitForSeconds (1);
+        Debug.Log("4");
+        yield return new WaitForSeconds (1);
+        Debug.Log("3");
+        yield return new WaitForSeconds (1);
+        Debug.Log("2");
+        yield return new WaitForSeconds (1);
+        Debug.Log("1");
+        yield return new WaitForSeconds (1);
+        SceneManager.LoadScene(3);
     }
 }
